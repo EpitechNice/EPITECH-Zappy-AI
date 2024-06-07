2ef31afdb7a1ac39e12e4c4fb2a3b3afaca2250a
H4rdeol <leonard.oursel@epitech.eu>
2024-06-07 17:37:59 +0200

    [+] Connection

--- src/Communication/Communication.cpp
+++ src/Communication/Communication.cpp
@@ -9,9 +9,27 @@
 #include "Communication.hpp"
 
 namespace IA {
+
+    Communication::CommunicationError::CommunicationError(const std::string &msg) : _msg(msg)
+    {}
+
+    const char *Communication::CommunicationError::what() const noexcept
+    {
+        return _msg.c_str();
+    }
+
     Communication::Communication(const std::string &ip, const int port)
     {
         _ip = ip;
         _port = port;
+        _socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (_socket == -1)
+            throw CommunicationError("Error: socket creation failed");
+        _server_addr.sin_family = AF_INET;
+        _server_addr.sin_port = htons(port);
+        if (inet_pton(AF_INET, ip.c_str(), &_server_addr.sin_addr) <= 0)
+            throw CommunicationError("Error: invalid address");
+        if (connect(_socket, (struct sockaddr *)&_server_addr, sizeof(_server_addr)) < 0)
+            throw CommunicationError("Error: connection failed");
     }
 }
--- src/Communication/Communication.hpp
+++ src/Communication/Communication.hpp
@@ -8,16 +8,31 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <memory>
 
 namespace IA
 {
-    class Communication
-    {
+    class Communication {
         public:
+            class CommunicationError : public std::exception {
+                public:
+                    CommunicationError(const std::string &msg) : _msg(msg) {}
+                    const char *what() const noexcept { return _msg.c_str(); }
+                private:
+                    std::string _msg;
+            };
+
             Communication(const std::string &ip, const int port);
             ~Communication() = default;
         private:
             std::string _ip;
             int _port;
+            int _socket;
+            struct sockaddr_in _server_addr;
     };
 }
